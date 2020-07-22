cff163b697419f65bde338d594cbf47acd0e259e
coffeemori92 <coffeemori92@naver.com>
2020-07-22 21:59:01 +0900

    Edit Nickname DONE

--- 01_front-end/components/EditNickname.js
+++ 01_front-end/components/EditNickname.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import useInput from '../customHooks/useInput';
+import { CHANGE_NICKNAME_REQUEST } from '../reducers/constants/user';
 import { Form } from '../styles/EditNicknameStyle';
 
 const EditNickname = () => {
   const dispatch = useDispatch();
-  const { me } = useSelector(state => state.user);
-  const [nickname, onChangeNickname] = useInput('');
+  const { changeNicknameDone } = useSelector(state => state.user);
+  const [nickname, onChangeNickname, setNickname] = useInput('');
+
+  useEffect(() => {
+    if(changeNicknameDone) {
+      setNickname('');
+    }
+  }, [changeNicknameDone]);
+  
+  const onSubmit = useCallback(e => {
+    e.preventDefault();
+    dispatch({
+      type: CHANGE_NICKNAME_REQUEST,
+      data: { nickname }
+    })
+  }, [nickname]);
+
   return (
     <>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <div>
-          <input 
+          <input
             value={nickname}
             onChange={onChangeNickname}
             placeholder="ニックネーム"
