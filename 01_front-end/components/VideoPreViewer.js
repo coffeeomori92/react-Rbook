0787d0f0a6d7441183ad45b0478c9d5102e27cc8
coffeemori92 <coffeemori92@naver.com>
2020-08-01 14:46:30 +0900

    remove upload video

--- 01_front-end/components/VideoPreViewer.js
+++ 01_front-end/components/VideoPreViewer.js
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { REMOVE_VIDEO } from '../reducers/constants/post';
+import { VIDDEO_URL } from '../config/config';
 import { VideoContainer, Header, HeaderFlex, Video } from '../styles/VideoPreViewerStyle';
 
 const VideoPreViewer = ({ video, index, setHaveVideo }) => {
